886554efa853a29d39fe24714e90c52737fef80b
Ed <seibeledouard@yahoo.fr>
2024-03-14 14:45:37 +0100

    Hello World !

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {props.message}
         </p>
         <a
           className="App-link"

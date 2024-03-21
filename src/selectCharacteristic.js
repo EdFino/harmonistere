8e7b8dc0a768f82b2f7141ff642c269ccbf50cdc
Ed <seibeledouard@yahoo.fr>
2024-03-21 15:55:54 +0100

    en guerre contre useState

--- src/selectCharacteristic.js
+++ src/selectCharacteristic.js
@@ -1,5 +1,47 @@
 import React, { useState } from 'react';
 
+function SelectCharacteristic () {
+
+    const [bodyValue, setBodyValue] = useState('');
+    const [mindValue, setMindValue] = useState('');
+    const [soulValue, setSoulValue] = useState('');
+    const [martialValue, setMartialValue] = useState('');
+    const [elementaryValue, setElementaryValue] = useState('');
+    const [speakingValue, setspeakingValue] = useState('');
+
+
+        const bodySelectChange = (e) => {
+        const bodyValue = e.target.value;
+        setBodyValue(bodyValue);
+        };
+
+        const mindSelectChange = (e) => {
+        const mindValue = e.target.value;
+        setMindValue(mindValue);
+        };
+
+        const soulSelectChange = (e) => {
+        const soulValue = e.target.value;
+        setSoulValue(soulValue);
+        };
+
+        const martialSelectChange = (e) => {
+        const martialValue = e.target.value;
+        setMartialValue(martialValue);
+        };
+
+    
+        const elementarySelectChange = (e) => {
+        const elementaryValue = e.target.value;
+        setElementaryValue(elementaryValue);
+        };
+
+        const speakingSelectChange = (e) => {
+        const speakingValue = e.target.value;
+        setSpeakingValue(speakingValue);
+        };
+
+
 const characList= [
     {value:'-1', description:'Malus -1'},
     {value:'0', description:'Neutre'},
@@ -7,34 +49,29 @@ const characList= [
     {value:'2', description:'Bonus critique +2'}
 ]
 
-function SelectCharacteristic() {
-  const [selectedValue, setSelectedValue] = useState('');
-
-  const handleSelectChange = (e) => {
-    const selectedValue = e.target.value;
-    setSelectedValue(selectedValue);
-    console.log(selectedValue);
-  };
+  function controlLevels () {
+ return null
+  }
 
   return (
     <div id='characForm'>
         <div id='inne'>
             <label htmlFor='body'>Corps</label>
-            <select id="body" name="body" value={selectedValue} onChange={handleSelectChange}>
+            <select id="body" name="body" value={bodyValue} onChange={bodySelectChange}>
                 <option value=''>Choisissez un élément dans la liste : </option>
                 {characList.map((element, index) => (
                 <option key={index} value={element.value}>{element.description}</option>
                 ))}
             </select>
             <label htmlFor='spirit'>Mental</label>
-            <select id="spirit" name="spirit" value={selectedValue} onChange={handleSelectChange}>
+            <select id="spirit" name="spirit" value={mindValue} onChange={mindSelectChange}>
                 <option value=''>Choisissez un élément dans la liste : </option>
                 {characList.map((element, index) => (
                     <option key={index} value={element.value}>{element.description}</option>
                 ))}
             </select>
             <label htmlFor='soul'>Âme</label>
-            <select id="soul" name="soul" value={selectedValue} onChange={handleSelectChange}>
+            <select id="soul" name="soul" value={soulValue} onChange={soulSelectChange}>
                 <option value=''>Choisissez un élément dans la liste : </option>
                 {characList.map((element, index) => (
                     <option key={index} value={element.value}>{element.description}</option>
@@ -43,27 +80,28 @@ function SelectCharacteristic() {
         </div>
         <div id='acquis'>
             <label htmlFor='martialArts'>Arts martiaux</label>
-                <select id="martialArts" name="martialArts" value={selectedValue} onChange={handleSelectChange}>
+                <select id="martialArts" name="martialArts" value={martialValue} onChange={marialSelectChange}>
                     <option value=''>Choisissez un élément dans la liste : </option>
                     {characList.map((element, index) => (
                     <option key={index} value={element.value}>{element.description}</option>
                     ))}
                 </select>
                 <label htmlFor='elementaryArts'>Arts élémentaires</label>
-                <select id="elementaryArts" name="elementaryArts" value={selectedValue} onChange={handleSelectChange}>
+                <select id="elementaryArts" name="elementaryArts" value={elementaryValue} onChange={elementarySelectChange}>
                     <option value=''>Choisissez un élément dans la liste : </option>
                     {characList.map((element, index) => (
                         <option key={index} value={element.value}>{element.description}</option>
                     ))}
                 </select>
                 <label htmlFor='publicSpeaking'>Arts oratoires</label>
-                <select id="publicSpeaking" name="publicSpeaking" value={selectedValue} onChange={handleSelectChange}>
+                <select id="publicSpeaking" name="publicSpeaking" value={speakingValue} onChange={speakingSelectChange}>
                     <option value=''>Choisissez un élément dans la liste : </option>
                     {characList.map((element, index) => (
                         <option key={index} value={element.value}>{element.description}</option>
                     ))}
                 </select>
         </div>
+        <button type='button' onClick={controlLevels}>Clique</button>
     </div>
     );
 }
